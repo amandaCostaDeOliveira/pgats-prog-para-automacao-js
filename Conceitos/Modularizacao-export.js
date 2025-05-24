a5888b649226af14dbcb1337659828d6b4347ad7
Amanda Oliveira <amandacoliveira95@gmail.com>
2025-05-24 15:13:35 -0300

    Conteúdo modularização

--- Conceitos/Modularizacao-export.js
+++ Conceitos/Modularizacao-export.js
@@ -3,6 +3,40 @@
  * modulo.exports = {} / require ('')    - deixou de ser usado (até 2020 mais ou menos)
  * 
  * ESM - Ecmascript Standard Modules     - mais usado vamos focar nela
- *  export {} / import
- * 
+ *  export {} / import {} from ''
  */
+
+//código
+console.log('Código')
+
+const nomePet1='Pitoco'
+console.log(`O nome do pet é ${nomePet1}`)
+
+const nomePet2='Pipoca'
+console.log(`O nome do pet é ${nomePet2}`)
+
+const nomePet3='Pantera'
+console.log(`O nome do pet é ${nomePet3}`)
+
+console.log('______________________')
+
+//função
+console.log('Função')
+
+function exibirNomePet(nomePet){
+    console.log(`O nome do pet é ${nomePet}`)
+}
+
+exibirNomePet('Pitoco')
+exibirNomePet('Pipoca')
+exibirNomePet('Pantera')
+
+console.log('_______________________')
+
+//Modulo Exportar - exportar função para uusar em outras partes do projeto
+//ESM: export {} 
+
+export {
+    exibirNomePet
+}
+
---
+++ Conceitos/Modularizacao-import.js
@@ -0,0 +1,6 @@
+//Modulo Import - importar função para usar aqui
+//ESM: import {} from ''
+
+import { exibirNomePet } from './Modularizacao-export.js'
+
+exibirNomePet('Importada')
