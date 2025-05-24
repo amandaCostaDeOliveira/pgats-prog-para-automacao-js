6921f75a14ae7d90d5e43ef7e59a3342d6b257eb
Amanda Oliveira <amandacoliveira95@gmail.com>
2025-05-24 13:44:08 -0300

    Corrigindo nome exercicios 7, 8 e 9

---
+++ Conceitos/Modularizacao-export.js
@@ -0,0 +1,8 @@
+/**
+ * Sintaxe CommonJS:
+ * modulo.exports = {} / require ('')    - deixou de ser usado (até 2020 mais ou menos)
+ * 
+ * ESM - Ecmascript Standard Modules     - mais usado vamos focar nela
+ *  export {} / import
+ * 
+ */
